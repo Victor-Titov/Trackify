0b803f697806f28ce545ee60316f6fa20a046ec2
VictorT <victor.titov.2009@gmail.com>
2025-06-07 01:45:45 +0300

    made models for person and habit

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from . import db
+from datetime import time
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Habit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    time = db.Column(db.Time)
+    desc = db.Column(db.Text)
+
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+    habits = db.relationship('Habit', backref='person', lazy=True)
+
+    
